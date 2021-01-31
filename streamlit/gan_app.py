962af2663f5f734b251bab2ebfe13da9dbec057f
Safiuddin Mohammad <40533383+safi842@users.noreply.github.com>
2021-01-31 14:08:33 +0530

    Create gan_app.py

---
+++ streamlit/gan_app.py
@@ -0,0 +1,45 @@
+import numpy as np
+import os
+import pickle
+import streamlit as st
+import sys
+import urllib
+import torch
+import biggan
+from torchvision.utils import make_grid
+
+def main():
+    st.title("Microstructure BigGAN-deep demo")
+    """This is a demonstration of conditional image generation of micrographs using [BigGAN-deep architecture](https://arxiv.org/abs/1809.11096)
+       The images generated are using three conditional inputs Annealing Temperature, Annealing Time and the type of cooling used.
+       GAN is trained using [UHCSDB](http://uhcsdb.materials.cmu.edu/) images"""
+    
+    st.sidebar.title('Processing Conditions',)
+    anneal_temp = st.sidebar.selectbox('Annealing Temperature °C',[700,750,800,900,970,1000,1100])
+    anneal_time = st.sidebar.selectbox('Annealing Time (M: Minutes, H: Hours)',['5M','90M','1H','3H','8H','24H','48H','85H'])
+    cooling = st.sidebar.selectbox('Cooling Type',['Quench','Furnace Cool','Air Cool','650C-1H'])
+    temp_dict = {970: 0, 800: 1, 900: 2, 1100: 3, 1000: 4, 700: 5, 750: 6}
+    time_dict = {'90M': 0, '24H': 1, '3H': 2, '5M': 3, '8H': 4, '85H': 5, '1H': 6, '48': 7}
+    cool_dict = {'Quench': 0, 'Air Cool': 1, 'Furnace Cool': 2, '650C-1H': 3}
+    model = load_gan()
+    noise = torch.randn(1,128)
+    image_out = generate_img(model, noise, temp_dict[anneal_temp], time_dict[anneal_time], cool_dict[cooling])
+    st.subheader('Generated Microstructure for the given processing conditions')
+    st.text("")
+    st.image(image_out, use_column_width=False)
+
+@st.cache(suppress_st_warning=True)
+def load_gan():
+    model = biggan.Generator()
+    model.load_state_dict(torch.load('BigGAN-deep.pth', map_location=torch.device('cpu')))
+    return model
+
+@st.cache(suppress_st_warning=True)
+def generate_img(model,noise, y_temp, y_time, y_cool):
+	y_temp = torch.tensor([y_temp])
+	y_time = torch.tensor([y_time])
+	y_cool = torch.tensor([y_cool])
+	with torch.no_grad():
+		synthetic = model(noise, y_temp, y_time, y_cool)
+	synthetic = make_grid(synthetic, normalize=True)
+	return np.transpose(synthetic.numpy() ,(1,2,0))
