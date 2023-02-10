cf3052678214c8996716db80dfbe9112bce10dde
Mohammad Safiuddin <40533383+safi842@users.noreply.github.com>
2023-02-10 22:30:42 +0530

    Add files via upload

---
+++ app/biggan.py
@@ -0,0 +1,194 @@
+import os
+import pandas as pd
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.utils.data import DataLoader, Dataset
+from torch.nn.utils import spectral_norm
+import torch.nn.functional as F
+import numpy as np
+from torchvision.utils import make_grid
+
+class ClassConditionalBN(nn.Module):
+    def __init__(self, input_size, output_size, eps=1e-4, momentum=0.1):
+        super(ClassConditionalBN, self).__init__()
+        self.output_size, self.input_size = output_size, input_size
+        # Prepare gain and bias layers
+        self.gain = spectral_norm(nn.Linear(input_size, output_size, bias = False), eps = 1e-4)
+        self.bias = spectral_norm(nn.Linear(input_size, output_size, bias = False), eps = 1e-4)
+        # epsilon to avoid dividing by 0
+        self.eps = eps
+        # Momentum
+        self.momentum = momentum
+        
+        self.register_buffer('stored_mean', torch.zeros(output_size))
+        self.register_buffer('stored_var',  torch.ones(output_size))
+    
+    def forward(self, x, y):
+        # Calculate class-conditional gains and biases
+        gain = (1 + self.gain(y)).view(y.size(0), -1, 1, 1)
+        bias = self.bias(y).view(y.size(0), -1, 1, 1)
+        out = F.batch_norm(x, self.stored_mean, self.stored_var, None, None,
+                          self.training, 0.1, self.eps)
+        return out * gain + bias
+    
+    def extra_repr(self):
+        s = 'out: {output_size}, in: {input_size},'
+        return s.format(**self.__dict__)
+
+class Self_Attn(nn.Module):
+    """ Self attention Layer"""
+    def __init__(self,in_dim,activation = nn.ReLU(inplace = False)):
+        super(Self_Attn,self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+        
+        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
+        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax  = nn.Softmax(dim=-1) #
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature 
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize,C,width ,height = x.size()
+        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B X CX(N)
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
+        energy =  torch.bmm(proj_query,proj_key) # transpose check
+        attention = self.softmax(energy) # BX (N) X (N) 
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
+
+        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        out = out.view(m_batchsize,C,width,height)
+        
+        out = self.gamma*out + x
+        return out
+
+class GeneratorResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, upsample = None, embed_dim = 128, dim_z = 384):
+        super(GeneratorResBlock, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.hidden_channels = self.in_channels // 4
+        
+        self.conv1 = spectral_norm(nn.Conv2d(self.in_channels, self.hidden_channels, kernel_size = 1, padding = 0), eps = 1e-4)
+        self.conv2 = spectral_norm(nn.Conv2d(self.hidden_channels, self.hidden_channels, kernel_size = 3, padding = 1), eps = 1e-4)
+        self.conv3 = spectral_norm(nn.Conv2d(self.hidden_channels, self.hidden_channels, kernel_size = 3, padding = 1), eps = 1e-4)
+        self.conv4 = spectral_norm(nn.Conv2d(self.hidden_channels, self.out_channels, kernel_size = 1, padding = 0), eps = 1e-4)
+        
+        self.bn1 = ClassConditionalBN((3 * embed_dim) + dim_z, self.in_channels)
+        self.bn2 = ClassConditionalBN((3 * embed_dim) + dim_z, self.hidden_channels)
+        self.bn3 = ClassConditionalBN((3 * embed_dim) + dim_z, self.hidden_channels)
+        self.bn4 = ClassConditionalBN((3 * embed_dim) + dim_z, self.hidden_channels)
+        
+        self.activation = nn.ReLU(inplace=False)
+        
+        self.upsample = upsample
+        
+    def forward(self,x,y):
+        # Project down to channel ratio
+        h = self.conv1(self.activation(self.bn1(x, y)))
+        # Apply next BN-ReLU
+        h = self.activation(self.bn2(h, y))
+        # Drop channels in x if necessary
+        if self.in_channels != self.out_channels:
+            x = x[:, :self.out_channels]      
+        # Upsample both h and x at this point  
+        if self.upsample:
+            h = self.upsample(h)
+            x = self.upsample(x)
+        # 3x3 convs
+        h = self.conv2(h)
+        h = self.conv3(self.activation(self.bn3(h, y)))
+        # Final 1x1 conv
+        h = self.conv4(self.activation(self.bn4(h, y)))
+        return h + x
+
+class Generator(nn.Module):
+    def __init__(self, G_ch = 64, dim_z = 384, bottom_width=4, img_channels = 1,
+                 init = 'N02',n_classes_temp = 7, n_classes_time = 8, n_classes_cool = 4, embed_dim = 128):
+        super(Generator, self).__init__()
+        self.ch = G_ch
+        self.dim_z = dim_z
+        self.bottom_width = bottom_width
+        self.init = init
+        self.img_channels = img_channels
+
+        self.embed_temp = nn.Embedding(n_classes_temp, embed_dim)
+        self.embed_time = nn.Embedding(n_classes_time, embed_dim)
+        self.embed_cool = nn.Embedding(n_classes_cool, embed_dim)
+        
+        self.linear = spectral_norm(nn.Linear(dim_z + (3 * embed_dim), 16 * self.ch * (self.bottom_width **2)), eps = 1e-4)
+        
+        self.blocks = nn.ModuleList([
+                GeneratorResBlock(16*self.ch, 16*self.ch),
+                GeneratorResBlock(16*self.ch, 16*self.ch, upsample =  nn.Upsample(scale_factor = 2)),
+                GeneratorResBlock(16*self.ch, 16*self.ch),
+                GeneratorResBlock(16*self.ch, 8*self.ch, upsample =  nn.Upsample(scale_factor = 2)),
+                GeneratorResBlock(8*self.ch, 8*self.ch),
+                GeneratorResBlock(8*self.ch, 8*self.ch, upsample =  nn.Upsample(scale_factor = 2)),
+                GeneratorResBlock(8*self.ch, 8*self.ch),
+                GeneratorResBlock(8*self.ch, 4*self.ch, upsample =  nn.Upsample(scale_factor = 2)),
+                Self_Attn(4*self.ch),
+                GeneratorResBlock(4*self.ch, 4*self.ch),
+                GeneratorResBlock(4*self.ch, 2*self.ch, upsample =  nn.Upsample(scale_factor = 2)),
+                GeneratorResBlock(2*self.ch, 2*self.ch),
+                GeneratorResBlock(2*self.ch,  self.ch, upsample =  nn.Upsample(scale_factor = 2))
+        ])
+        
+        self.final_layer = nn.Sequential(
+                nn.BatchNorm2d(self.ch),
+                nn.ReLU(inplace = False),
+                spectral_norm(nn.Conv2d(self.ch, self.img_channels, kernel_size = 3, padding = 1)),
+                nn.Tanh()
+        )
+        
+        self.init_weights()
+                                    
+    def init_weights(self):
+        print(f"Weight initialization : {self.init}")
+        self.param_count = 0
+        for module in self.modules():
+            if (isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear) or isinstance(module, nn.Embedding)):
+                if self.init == 'ortho':
+                    torch.nn.init.orthogonal_(module.weight)
+                elif self.init == 'N02':
+                    torch.nn.init.normal_(module.weight, 0, 0.02)
+                elif self.init in ['glorot', 'xavier']:
+                    torch.nn.init.xavier_uniform_(module.weight)
+                else:
+                    print('Init style not recognized...')
+                self.param_count += sum([p.data.nelement() for p in module.parameters()])
+        #print("Param count for G's initialized parameters: %d Million" % (self.param_count/1000000))
+        
+        
+    def forward(self,z , y_temp, y_time, y_cool):
+        y_temp = self.embed_temp(y_temp)
+        y_time = self.embed_time(y_time)
+        y_cool = self.embed_cool(y_cool)
+        z = torch.cat([z, y_temp, y_time, y_cool], 1)     
+        # First linear layer
+        h = self.linear(z)
+        # Reshape
+        h = h.view(h.size(0), -1, self.bottom_width, self.bottom_width)    
+        # Loop over blocks
+        for i, block in enumerate(self.blocks):
+            if i != 8:
+                h = block(h, z)
+            else:
+                h = block(h)
+        # Apply batchnorm-relu-conv-tanh at output
+        h = self.final_layer(h)
+        return h
---
+++ app/gan_app.py
@@ -0,0 +1,112 @@
+import numpy as np
+import os
+import json
+import requests
+#import urllib.request
+from stqdm import stqdm
+from PIL import Image
+import pickle
+import streamlit as st
+import sys
+import urllib
+import torch
+import random
+import biggan
+from torchvision.utils import make_grid
+from io import BytesIO
+import base64
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+def main():
+    first_run = not os.path.exists('state.json') 
+    state = {}
+    st.title("Microstructure GAN demo")
+    """This is a demonstration of conditional image generation of micrographs using a modified architecture based on [BigGAN-deep](https://arxiv.org/abs/1809.11096)
+       The images generated are using three conditional inputs Annealing Temperature, Annealing Time and the type of cooling used.
+       GAN is trained using [Omni Loss](https://arxiv.org/abs/2011.13074) on [UHCSDB](http://uhcsdb.materials.cmu.edu/) images. Details on the methodology can be found in the [paper](https://arxiv.org/abs/2107.09402)"""
+    
+    st.sidebar.title('Processing Conditions',)
+    state['anneal_temp'] = st.sidebar.selectbox('Annealing Temperature °C',[700,750,800,900,970,1000,1100])
+    state['anneal_time'] = st.sidebar.selectbox('Annealing Time (M: Minutes, H: Hours)',['5M','90M','1H','3H','8H','24H','48H','85H'])
+    state['cooling'] = st.sidebar.selectbox('Cooling Type',['Quench','Furnace Cool','Air Cool','650C-1H'])
+    temp_dict = {970: 0, 800: 1, 900: 2, 1100: 3, 1000: 4, 700: 5, 750: 6}
+    time_dict = {'90M': 0, '24H': 1, '3H': 2, '5M': 3, '8H': 4, '85H': 5, '1H': 6, '48H': 7}
+    cool_dict = {'Quench': 0, 'Air Cool': 1, 'Furnace Cool': 2, '650C-1H': 3}
+    model = load_gan()
+    st.sidebar.subheader('Generate a new latent Vector')
+    state['seed'] =  7
+    if st.sidebar.button('New z'):
+        state['seed'] = random.randint(0,1000)
+    rng = np.random.RandomState(state['seed'])
+    noise = torch.tensor(rng.normal(0, 1, (1, 384))).float()
+    state['noise'] = noise.numpy()
+    y_temp = temp_dict[state['anneal_temp']]
+    y_time = time_dict[state['anneal_time']]
+    y_cool = cool_dict[state['cooling']]
+
+    state['image_out'] = generate_img(model, noise, y_temp, y_time, y_cool)
+    st.subheader('Generated Microstructure for the given processing conditions')
+    st.text("")
+    st.text(f"Random seed: {state['seed']}")
+    st.image(np.array(state['image_out']), use_column_width=False)
+
+    save_bool = st.button('Save Image')
+    if save_bool:
+        with open('state.json', 'r') as fp:
+            state_old = json.load(fp)
+        st.text(f"The following image was saved. It was generated using a random seed: {state_old['seed']}")
+        st.image(np.array(state_old['image_out']), use_column_width=False)
+        if not os.path.exists('Generated Micrographs'):
+            os.makedirs('Generated Micrographs')
+        im = Image.fromarray((np.array(state_old['image_out']).reshape(256,256) * 255).astype(np.uint8))
+        im.save(f"./Generated Micrographs/{state_old['anneal_temp']}-{state_old['anneal_time']}-{state_old['cooling']}-{state_old['seed']}.png")
+    
+    state['save_bool'] = save_bool
+    with open('state.json', 'w') as fp:
+        json.dump(state, fp, cls=NumpyEncoder)
+        
+def download_model(url, filename):
+    """
+    Helper method handling downloading large files from `url` to `filename`. Returns a pointer to `filename`.
+    """
+    with st.spinner(text= f"Downloading {filename} ..."):
+        chunkSize = 1024
+        r = requests.get(url, stream=True)
+        with open(filename, 'wb') as f:
+            pbar = stqdm( unit="B", total=int( r.headers['Content-Length'] ) )
+            for chunk in r.iter_content(chunk_size=chunkSize): 
+                if chunk: # filter out keep-alive new chunks
+                    pbar.update (len(chunk))
+                    f.write(chunk)
+
+@st.cache(suppress_st_warning=True)
+def load_gan():
+    if not os.path.isfile("BigGAN-deep.pth"):
+        url = "https://github.com/safi842/Microstructure-GAN/releases/download/v0/BigGAN-deep.pth"
+        filename = "BigGAN-deep.pth"
+        download_model(url, filename)
+        st.write(f"Downloaded {filename}...")
+        #filename, headers = urllib.request.urlretrieve(url,"BigGAN-deep.pth",MyProgressBar())
+    model = biggan.Generator()
+    model.load_state_dict(torch.load('BigGAN-deep.pth', map_location=torch.device('cpu')))
+    return model
+
+@st.cache(suppress_st_warning=True)
+def generate_img(model,noise, y_temp, y_time, y_cool):
+	y_temp = torch.tensor([y_temp])
+	y_time = torch.tensor([y_time])
+	y_cool = torch.tensor([y_cool])
+	with torch.no_grad():
+		synthetic = model(noise, y_temp, y_time, y_cool)[0]
+		synthetic = 0.5 * synthetic + 0.5
+	#synthetic = make_grid(synthetic, normalize=True)
+	return np.transpose(synthetic.numpy() ,(1,2,0))
+
+main()
+st.markdown('<div style="text-align: center;">Copyright (c) 2023 Mohammad Safiuddin</div>', unsafe_allow_html=True)
